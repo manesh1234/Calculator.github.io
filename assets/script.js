bb9f212f7670e77efd6b1b664eb6374aa54a57ec
Manesh Ram <74581915+manesh1234@users.noreply.github.com>
2022-01-28 16:43:12 +0530

    add files via upload

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+/* THIS IS ONLY FOR VALIDATING INPUT ALL THE LOGICS ARE IN HTML INLINE JAVASCRIPT  */
+
+function validation(e){
+    if(e.key>=0 && e.key<=9){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
