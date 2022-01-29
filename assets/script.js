ffe57cad8fecc8a0e54142ddfce0c4759550970f
Manesh Ram <74581915+manesh1234@users.noreply.github.com>
2022-01-29 23:35:48 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,21 @@
 /* THIS IS ONLY FOR VALIDATING INPUT ALL THE LOGICS ARE IN HTML INLINE JAVASCRIPT  */
 
-var el = document.querySelector('#inp');
-function validation(e){
-    if(e.key=='Enter'){
-        e.preventDefault(); 
-        var val = el.value;
-        let ans,t="";   
-        for(var i=0;i<val.length;i++){
-            if(val[i]!='+'){
-                t += val[i];
-                console.log(t);
-            }
-            if(val[i]=='+'){
-                var r = parseInt(t);
-                console.log(typeof(r));
-                ans += r;
-                console.log(ans);
-                t = "";
-            }
-        }
-        console.log(typeof(ans));
-        console.log(ans);
-        console.log(typeof(t));
-
-        el.innerHTML = el.value;
+var input_element = document.querySelector('#inp');
+function validation(e) {
+    if (e.key == 'Enter') {
+        e.preventDefault();
+        var val = input_element.value;
+        ans = eval(val);  // EVAL() function which is inbuilt function used to perform all arithmatic operations on given input 'string' and return output of 'int' result 
+        input_element.value = ans;
         return true;
     }
-    if(e.key>=0 && e.key<=9){
+    if (e.key >= 0 && e.key <= 9) {
         return true;
     }
-    else if(e.key=='%' ||  e.key=='/' || e.key=='*' || e.key=='-' || e.key=='+'){
+    else if (e.key == '%' || e.key == '/' || e.key == '*' || e.key == '-' || e.key == '+') {
         return true;
     }
-    else{
+    else {
         return false;
     }
 }
